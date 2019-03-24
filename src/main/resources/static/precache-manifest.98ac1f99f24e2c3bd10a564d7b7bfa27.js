8fbfd098f82a4d987376122bbf6b41a569e920b0
kuzink <kuzink11@gmail.com>
2019-03-24 22:20:55 +0300

    add react

---
+++ src/main/resources/static/precache-manifest.98ac1f99f24e2c3bd10a564d7b7bfa27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2cc29b307cb19714a6cd",
+    "url": "/static/js/main.2cc29b30.chunk.js"
+  },
+  {
+    "revision": "bfad2e3700006bab11a6",
+    "url": "/static/js/2.bfad2e37.chunk.js"
+  },
+  {
+    "revision": "2cc29b307cb19714a6cd",
+    "url": "/static/css/main.c227c2de.chunk.css"
+  },
+  {
+    "revision": "bfad2e3700006bab11a6",
+    "url": "/static/css/2.d38bf6a2.chunk.css"
+  },
+  {
+    "revision": "ceb2af05a00d9f8869c4f850f5f967f3",
+    "url": "/index.html"
+  }
+];
