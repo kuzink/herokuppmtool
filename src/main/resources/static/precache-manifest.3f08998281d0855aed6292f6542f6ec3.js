2330b21b74b111608b7d1f035a1602b22477e802
kuzink <kuzink11@gmail.com>
2019-03-26 20:25:19 +0300

    sfyhgk

---
+++ src/main/resources/static/precache-manifest.3f08998281d0855aed6292f6542f6ec3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7731841190102de6039933c6b2467abe",
+    "url": "/static/media/girl.77318411.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "56145e2066a1db0a8890",
+    "url": "/static/js/main.56145e20.chunk.js"
+  },
+  {
+    "revision": "3f3fc75d3c6ad2d59a07",
+    "url": "/static/js/2.3f3fc75d.chunk.js"
+  },
+  {
+    "revision": "56145e2066a1db0a8890",
+    "url": "/static/css/main.c1a8c36d.chunk.css"
+  },
+  {
+    "revision": "3f3fc75d3c6ad2d59a07",
+    "url": "/static/css/2.d38bf6a2.chunk.css"
+  },
+  {
+    "revision": "7e492289c315d1d41a304d590a3ca62b",
+    "url": "/index.html"
+  }
+];
